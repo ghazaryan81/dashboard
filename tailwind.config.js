27a100da2155977c51da7695c3190312821038d6
Hayk Ghazaryan <hovhannesi.hayk.ghazaryan.81@gmail.com>
2023-05-04 21:18:05 +0400

    feat(tailwindcss) add in tailwind.config.js location watching to selectors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: [],
+	content: [
+		"./src/scripts/react-ts/components/**/*.{js,ts,jsx,tsx}",
+		"./src/scripts/react/components/**/*.{js,ts,jsx,tsx}",
+		"./src/pug/**/*.{jade,pug}",
+		"./src/html/**/*.{htm,html}",
+	],
 	theme: {
 		extend: {},
 	},
